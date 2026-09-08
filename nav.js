734f27dceb51fcd947113ed76d49163560f0a60c
andrewjh1436 <andrewjh@iastate.edu>
2026-09-08 13:12:18 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,19 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const nav = document.createElement("nav");
 
-  nav.innerHTML = `
-    <div class="nav-container">
-      <a href="index.html">About Me</a>
-      <a href="research.html">Research</a>
-      <a href="teaching.html">Teaching</a>
-      <a href="personal.html">Personal</a>
-    </div>
-  `;
+    const navigation = document.getElementById("navigation");
 
-  document.body.prepend(nav);
+    if (!navigation) {
+        return;
+    }
+
+    navigation.innerHTML = `
+        <nav class="primary-nav">
+
+            <a href="index.html">
+                Home
+            </a>
+
+            <a href="about.html">
+                About Me
+            </a>
+
+            <a href="research.html">
+                Research
+            </a>
+
+            <a href="teaching.html">
+                Teaching
+            </a>
+
+            <a href="personal.html">
+                Personal
+            </a>
+
+        </nav>
+    `;
+
+
+    /* Highlight the page the user is currently on */
+
+    const currentPage =
+        window.location.pathname.split("/").pop() || "index.html";
+
+    const navLinks =
+        document.querySelectorAll(".primary-nav a");
+
+    navLinks.forEach(function (link) {
+
+        const linkPage =
+            link.getAttribute("href");
+
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+
+    });
+
 });
-
-  <script src="nav.js"></script>
-
-</body>
-</html>
