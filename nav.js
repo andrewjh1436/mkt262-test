c979bc56b75e63cb099f1dc0bde9ca2a39e5319e
andrewjh1436 <andrewjh@iastate.edu>
2026-09-10 13:19:38 -0500

    fix nav.js

--- nav.js
+++ nav.js
@@ -9,45 +9,39 @@ document.addEventListener("DOMContentLoaded", function () {
     navigation.innerHTML = `
         <nav class="primary-nav">
 
-            <a href="index.html">
-                Home
-            </a>
+            <a href="index.html">Home</a>
 
-            <a href="about.html">
-                About Me
-            </a>
+            <a href="about.html">About Me</a>
 
-            <a href="research.html">
-                Research
-            </a>
+            <a href="research.html">Research</a>
 
-            <a href="teaching.html">
-                Teaching
-            </a>
+            <a href="teaching.html">Teaching</a>
 
-            <a href="personal.html">
-                Personal
-            </a>
+            <a href="personal.html">Personal</a>
 
         </nav>
     `;
 
 
-    /* Highlight the page the user is currently on */
+    // Find the current page
+    let currentPage = window.location.pathname.split("/").pop();
 
-    const currentPage =
-        window.location.pathname.split("/").pop() || "index.html";
 
-    const navLinks =
-        document.querySelectorAll(".primary-nav a");
+    // GitHub Pages may leave the homepage blank
+    if (currentPage === "") {
+        currentPage = "index.html";
+    }
+
+
+    // Highlight the current page
+    const navLinks = document.querySelectorAll(".primary-nav a");
 
     navLinks.forEach(function (link) {
 
-        const linkPage =
-            link.getAttribute("href");
+        if (link.getAttribute("href") === currentPage) {
 
-        if (linkPage === currentPage) {
             link.classList.add("active");
+
         }
 
     });
